89f75a02c14d0269e6193028ab6c1bb016b30c23
Gopinath Shiva <sgopinath31@gmail.com>
2016-04-13 09:04:55 +0530

    Update aff-link-generation.js

--- aff-link-generation.js
+++ aff-link-generation.js
@@ -1,10 +1,109 @@
 var macro = '';
 var filterImage = "FILTER TYPE=IMAGES STATUS=ON "+"\n";
 
-var csvfiles = ['grabon-thegudlook-urls-3_30_2016-9_02_38AM'];
+var csvObj = { 
+  pepperfry:
+   { csvFile: 'output-csv/grabon-pepperfry-urls-4_7_2016-3_49_37PM.csv',
+     rowCount: 24 },
+  zivame:
+   { csvFile: 'output-csv/grabon-zivame-urls-4_11_2016-8_51_53AM.csv',
+     rowCount: 27 } 
+};
 
-var affBtnIDS = ['907'];
-var offerCounts= [9];
+var blacklistStores = ["ace2three", "amazon", "askmebazaar", "askmegrocery", "candere", "ezone", "flipkart",
+ "foodpanda", "freecharge", "happilyunmarried", "jockey","makemytrip", "mobikwik", "naaptol", "printvenue",
+  "purplle", "snapdeal", "syberplace", "thomascook"];
+
+var csvfiles = [];
+var offerCounts = [];
+var affBtnIDS = [];
+
+var affBtnObj = {
+	faballey:28,
+	makemytrip:44, //seperate btn ids for flight
+	pepperfry:52,
+	printland:90,
+	ticketgoose:98,
+	zivame:100,
+	vistaprint:114,
+	lenskart:120,
+	shopclues:122,
+	jabong:126,
+	trendin:140,
+	clovia:146,
+	stalkbuylove:230,
+	nykaa:232,
+	wonderchef:244,
+	fernsnpetals:260,
+	limeroad:280,
+	shopcj:290,
+	koovs:318,
+	healthgenie:346,
+	suratdiamond:364,
+	bookmyflowers:398,
+	homeshop18:402,
+	zovi:480,
+	shoppersstop:510,
+	dailyobjects:542,
+	smiledrive:571,
+	mybustickets:579,
+	indiangiftsportal:709,
+	bluestone:713,
+	archies:727,
+	chumbak:735,
+	expedia:779,
+	floweraura:787,
+	americanswan:817,
+	infibeam:899,
+	thegudlook:907,
+	indiareads:919,
+	shopnineteen:985,
+	excitinglives:1006,
+	travelkhana:1016,
+	ebay:1018,
+	paytm:1022,
+	fabfurnish:1070,
+	kapkids:1078,
+	giftease:1126,
+	amzer:1236,
+	giftsbymeeta:1274,
+	giftalove:1292,
+	fabindia:1326,
+	oyorooms:1362,
+	evok:1460,
+	healthkart:1480,
+	bilba:1490,
+	"1mg":1578,
+	agoda:1632,
+	tolexo:1670,
+	greendust:1678,
+	voylla:1686,
+	wearyourshine:1700,
+	myflowertree:1720,
+	aplava:1764,
+	kazo:1792,
+	bluehost:1960,
+	firstcry:2031,
+	flyingmachine:2141,
+	industrybuying:2187,
+	abof:2361,
+	naturesbasket:2371,
+	sendmygift:2375
+};
+
+var unavailableAffBtnIds = [];
+
+for(var c in csvObj){
+	if(blacklistStores.indexOf(csvObj[c])<0){
+		if(!affBtnObj[c]){
+			unavailableAffBtnIds.push(c);
+		}else{
+			affBtnIDS.push(affBtnObj[c]);
+			csvfiles.push(csvObj[c].csvFile);
+			offerCounts.push(csvObj[c].rowCount);
+		}
+	}
+}
 
 var sitename = 'grabon';
 
@@ -18,6 +117,7 @@ for(var j = 0; j < csvfiles.length; j++){
 
 	var offerCount = offerCounts[j];
 	var csvfile = csvfiles[j];
+	csvfile = csvfile.split('/')[1];
 
 	var storename = csvfile.split('-');
 	storename = storename[1];
@@ -35,6 +135,7 @@ for(var j = 0; j < csvfiles.length; j++){
 	macro += "ADD !EXTRACT EXPIRY"+"\n";
 	macro += "ADD !EXTRACT CODE"+"\n";
 	macro += "ADD !EXTRACT URL"+"\n";
+	macro += "ADD !EXTRACT CATEGORIES"+"\n";
 	macro += "ADD !EXTRACT CONVERTED<SP>AFF<SP>URL"+"\n";
 
 	macro += "SAVEAS TYPE=EXTRACT FOLDER=* FILE="+filename+".csv";
@@ -48,7 +149,11 @@ for(var j = 0; j < csvfiles.length; j++){
 
 		//macro += "SET !SINGLESTEP YES "+"\n";
 
-		macro += "SET !DATASOURCE "+csvfile+".csv"+"\n";
+		if(csvfile.indexOf('.csv')<0){
+			macro += "SET !DATASOURCE "+csvfile+".csv"+"\n";
+		}else{
+			macro += "SET !DATASOURCE "+csvfile+"\n";
+		}
 
 		iimDisplay("Generating Affiliate link no:"+i);
 
@@ -79,6 +184,8 @@ for(var j = 0; j < csvfiles.length; j++){
 		macro += "ADD !EXTRACT {{!COL6}}"+"\n";
 
 		macro += "ADD !EXTRACT {{!COL7}}"+"\n";
+
+		macro += "ADD !EXTRACT {{!COL8}}"+"\n";
 
 		macro += "WAIT SECONDS=1"+"\n";
 
